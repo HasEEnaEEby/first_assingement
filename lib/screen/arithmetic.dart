0f1bfc368afce4a9db979bcff0d83f8182d9dc65
hasee0001 <kch14570@gmail.com>
2024-11-26 14:04:10 +0545

    Addition

--- lib/screen/arithmetic.dart
+++ lib/screen/arithmetic.dart
@@ -1,9 +1,47 @@
 import 'package:flutter/material.dart';
 
-class Arithmetic extends StatelessWidget {
+class Arithmetic extends StatefulWidget {
+  const Arithmetic({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _ArithmeticState createState() => _ArithmeticState();
+}
+
+class _ArithmeticState extends State<Arithmetic> {
   final TextEditingController num1Controller = TextEditingController();
   final TextEditingController num2Controller = TextEditingController();
   String result = "";
+
+  void performOperation(String operation) {
+    double num1 = double.tryParse(num1Controller.text) ?? 0.0;
+    double num2 = double.tryParse(num2Controller.text) ?? 0.0;
+
+    double calcResult;
+
+    switch (operation) {
+      case 'add':
+        calcResult = num1 + num2;
+        result = "Sum: ${calcResult.toStringAsFixed(2)}";
+        break;
+      case 'subtract':
+        calcResult = num1 - num2;
+        result = "Difference: ${calcResult.toStringAsFixed(2)}";
+        break;
+      case 'multiply':
+        calcResult = num1 * num2;
+        result = "Product: ${calcResult.toStringAsFixed(2)}";
+        break;
+      case 'divide':
+        calcResult = num2 != 0 ? num1 / num2 : double.infinity;
+        result = num2 != 0
+            ? "Division: ${calcResult.toStringAsFixed(2)}"
+            : "Cannot divide by zero";
+        break;
+    }
+
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,45 +68,28 @@ class Arithmetic extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
                 ElevatedButton(
-                  onPressed: () {
-                    double num1 = double.parse(num1Controller.text);
-                    double num2 = double.parse(num2Controller.text);
-                    double sum = num1 + num2;
-                    result = "Sum: \$${sum.toStringAsFixed(2)}";
-                  },
+                  onPressed: () => performOperation('add'),
                   child: const Text('Add'),
                 ),
                 ElevatedButton(
-                  onPressed: () {
-                    double num1 = double.parse(num1Controller.text);
-                    double num2 = double.parse(num2Controller.text);
-                    double diff = num1 - num2;
-                    result = "Difference: \$${diff.toStringAsFixed(2)}";
-                  },
+                  onPressed: () => performOperation('subtract'),
                   child: const Text('Subtract'),
                 ),
                 ElevatedButton(
-                  onPressed: () {
-                    double num1 = double.parse(num1Controller.text);
-                    double num2 = double.parse(num2Controller.text);
-                    double prod = num1 * num2;
-                    result = "Product: \$${prod.toStringAsFixed(2)}";
-                  },
+                  onPressed: () => performOperation('multiply'),
                   child: const Text('Multiply'),
                 ),
                 ElevatedButton(
-                  onPressed: () {
-                    double num1 = double.parse(num1Controller.text);
-                    double num2 = double.parse(num2Controller.text);
-                    double div = num1 / num2;
-                    result = "Division: \$${div.toStringAsFixed(2)}";
-                  },
+                  onPressed: () => performOperation('divide'),
                   child: const Text('Divide'),
                 ),
               ],
             ),
             const SizedBox(height: 20),
-            Text(result),
+            Text(
+              result,
+              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
           ],
         ),
       ),
